95c33602794e40274fdaf33b2175b151444b3f6f
robert bulliard <bbulliard@gmail.com>
2021-10-21 10:24:44 -0500

    Initial Commit

---
+++ sully/sullyApp.swift
@@ -0,0 +1,17 @@
+//
+//  sullyApp.swift
+//  sully
+//
+//  Created by Robert Bulliard on 10/21/21.
+//
+
+import SwiftUI
+
+@main
+struct sullyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
